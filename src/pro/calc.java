97744a4e6eaa0cb30d8185e6c1dbb667d828cb0c
User <User@172.19.203.201>
2020-03-02 14:43:51 +0200

    wff

--- src/pro/calc.java
+++ src/pro/calc.java
@@ -2,12 +2,8 @@ package pro;
 
 public class calc {
 
-	public calc() {
-		// TODO Auto-generated constructor stub
-	}
 	
 	public int add(int i, int j) {
-		// TODO Auto-generated method stub
 		return i+j;
 	}
 
